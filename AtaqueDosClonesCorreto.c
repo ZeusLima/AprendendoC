9ef255517545c233215774509690db0b1b02a7bd
ZeusLima <zeuslima360@hgmail.com>
2022-09-28 19:42:09 -0300

    finalizando questão jedi

--- AtaqueDosClonesCorreto.c
+++ AtaqueDosClonesCorreto.c
@@ -4,14 +4,16 @@ int main(){
 
     int N, M, Jx, Jy;
 
-printf("INSIRA O TAMANHO DO MAPA\n"); scanf("%d %d", &N, &M);
+///printf("INSIRA O TAMANHO DO MAPA\n");
+scanf("%d %d", &N, &M);
     
     int mapa[N+2][M+2];
     int mapaPicos[N+2][M+2];
     int FlagResgate = 0;
     int cont = 0; ///contará as impressões de locais de pico de radiação
 
-printf("\nINSIRA AS UULTIMAS COORDENADAS DO JEDI: \n"); scanf("%d %d", &Jx, &Jy);
+///printf("\nINSIRA AS UULTIMAS COORDENADAS DO JEDI: \n"); 
+scanf("%d %d", &Jx, &Jy);
 
 
 ///zerando mapas
@@ -21,7 +23,7 @@ printf("\nINSIRA AS UULTIMAS COORDENADAS DO JEDI: \n"); scanf("%d %d", &Jx, &Jy)
             mapaPicos[i][j] = 0;
         }
     }
-printf("\nINSIRA O MAPA DE RADIACAO: \n");
+///printf("\nINSIRA O MAPA DE RADIACAO: \n");
 
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
@@ -33,8 +35,8 @@ printf("\nINSIRA O MAPA DE RADIACAO: \n");
     for(int i = 1; i < N+2; i++){
         for(int j = 1; j < M+2; j++){
             if( mapa[i][j] > mapa[i-1][j-1] && mapa[i][j] > mapa[i-1][j] && mapa[i][j] > mapa[i-1][j+1] &&
-                mapa[i][j] > mapa[i-1][j]                                && mapa[i][j] > mapa[i][j+1] &&
-                mapa[i][j] > mapa[i-1][j-1] && mapa[i][j] > mapa[i+1][j] && mapa[i][j] > mapa[i+1][j+1])
+                mapa[i][j] > mapa[i][j-1]                                && mapa[i][j] > mapa[i][j+1] &&
+                mapa[i][j] > mapa[i+1][j-1] && mapa[i][j] > mapa[i+1][j] && mapa[i][j] > mapa[i+1][j+1])
                 {
                 mapaPicos[i][j] = 1;
                 }
@@ -50,7 +52,7 @@ printf("\nINSIRA O MAPA DE RADIACAO: \n");
                 printf("Local %d: %d %d\n",cont, i, j);
 
                 if(i == Jx && j == Jy){
-                    printf("\n\n MORREU SENDO ATRIBUÍDO VERDADEIRO\n");
+                    ///printf("\n\n MORREU SENDO ATRIBUÍDO VERDADEIRO\n");
                     FlagResgate = 0;
                 }else{
                     FlagResgate = 1;
@@ -64,11 +66,11 @@ printf("\nINSIRA O MAPA DE RADIACAO: \n");
     if(FlagResgate == 1){
         printf("Ao resgate!\n");
     }else{
-        printf("Desanse na Força\n");
+        printf("Desanse na Força...");
     }
     
-
-    printf("\n\n\nXXXXXXXXXX testando impressões de mapa XXXXXXXXXXXXXX");
+/*
+   printf("\n\n\nXXXXXXXXXX testando impressões de mapa XXXXXXXXXXXXXX");
     printf("\nIMPRIMINDO MAPA: \n");
 
     for(int i = 0; i < N+2; i++){
@@ -84,7 +86,7 @@ printf("\nINSIRA O MAPA DE RADIACAO: \n");
             printf("\t%d", mapaPicos[i][j]);
         }printf("\n");
     }
-
+*/
 
 
 return 0;
