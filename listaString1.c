10fa26f26e820f80006054cb6fe137f841333e53
Zeus Lima <zeuslima360@gmail.com>
2022-10-17 14:02:27 -0300

    Respondendo a lista1

---
+++ listaString1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void CamelCase(int tam, char stringzada[tam]){
+
+    for(int i = 0; i < 60; i++){
+        
+        stringzada[0] = toupper(stringzada[0]);
+
+        if(stringzada[i] == ' '){
+            stringzada[i+1] = toupper(stringzada[i+1]);
+        }
+    }
+
+    printf("%s", stringzada);
+
+
+}
+
+int main(){
+    int tamanho = 61;
+    char texto[tamanho];
+    
+    fgets(texto, 60, stdin);
+
+    CamelCase(tamanho, texto);
+
+    
+
+
+
+
+
+    return 0;
+}
